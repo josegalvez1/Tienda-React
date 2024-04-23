dfe9057e26c2a8d3d5ea20c938acb3613c91fa57
josegalvez1 <josegalvez11@gmail.com>
2024-04-23 02:22:28 +0200

    first commit

---
+++ 06-shopping-cart/src/App.jsx
@@ -0,0 +1,11 @@
+import { products } from './mocks/products.json'
+import { Products } from './components/Products.jsx'
+
+function App () {
+
+  return (
+      <Products products={products} />
+  )
+}
+
+export default App
